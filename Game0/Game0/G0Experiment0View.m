5c132ac7bd632c37cc657de7b5dea23c06ee3fe9
Simon Strandgaard <neoneye@gmail.com>
2013-12-10 19:03:56 +0100

    Added a tap gesture recognizer

--- Game0/Game0/G0Experiment0View.m
+++ Game0/Game0/G0Experiment0View.m
@@ -7,15 +7,53 @@
 
 #import "G0Experiment0View.h"
 
+
+@interface G0Experiment0View () <UIGestureRecognizerDelegate>
+
+@property (nonatomic, strong) UITapGestureRecognizer *tapGestureRecognizer;
+
+@end
+
 @implementation G0Experiment0View
 
 - (id)init {
     self = [super init];
     if (self) {
-        // Initialization code
+		[self installGestures];
     }
     return self;
 }
+
+-(void)installGestures {
+	self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
+    [self addGestureRecognizer:self.tapGestureRecognizer];
+    self.tapGestureRecognizer.delegate = self;
+}
+
+-(void)tapAction:(UITapGestureRecognizer*)recognizer {
+    if ([recognizer state] == UIGestureRecognizerStateEnded) {
+        CGPoint loc = [recognizer locationInView:self];
+        //DLog(@"loc: %@", NSStringFromCGPoint(loc));
+        
+        CGRect bounds = self.bounds;
+        //DLog(@"bounds: %@", NSStringFromCGSize(bounds.size));
+        
+        float aspect = bounds.size.height / bounds.size.width;
+        float inv_aspect = 1.f / aspect;
+        //DLog(@"aspect: %.2f", aspect);
+        
+        float x = loc.x / bounds.size.width;
+        float y = loc.y / bounds.size.height;
+        x = x * 2.f - 1.f;
+        y = y * 2.f - 1.f;
+        
+        x *= inv_aspect;
+        y *= -1.f;
+		
+		NSLog(@"xy : %.2f %.2f", x, y);
+	}
+}
+
 
 - (void)drawRect:(CGRect)rect {
 	[[UIColor redColor] setFill];
